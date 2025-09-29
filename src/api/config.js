d80b61a068705d3f83b62ea529b52d2db73431ca
joeltestings <joel.casado@inslapineda.cat>
2025-09-29 11:40:59 +0200

    alalal

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 
-const LAN_IP = "192.168.50.125"; // la IP que acabas de comprobar
+const LAN_IP = "10.134.229.189"; // la IP que acabas de comprobar
 const PORT = 4000;
 
 const host = Platform.select({
