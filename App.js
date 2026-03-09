bf0f8c89fd382b0de0211a9efcca07c72a85856c
Sergicruga <sergicruga@gmail.com>
2026-03-09 10:30:48 +0100

    Arreglados imports

--- App.js
+++ App.js
@@ -6,13 +6,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthProvider from './src/context/AuthContext';
 import { requestNotificationPermission } from './src/utils/notifications';
 import * as Notifications from 'expo-notifications';
-import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator';
-import { EventProvider } from './src/EventContext';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AuthProvider from './src/context/AuthContext';
-import { requestNotificationPermission } from './src/utils/notifications';
-import * as Notifications from 'expo-notifications';
 
 export default function App() {
   Notifications.setNotificationHandler({
