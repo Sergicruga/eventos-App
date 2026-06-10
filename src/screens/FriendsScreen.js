2da310eb3481139bd0668f33c55d7aaa5d84825f
jgeneration <jgeneration@proton.me>
2026-06-10 12:11:50 +0200

    Arreglo Amigos

--- src/screens/FriendsScreen.js
+++ src/screens/FriendsScreen.js
@@ -18,6 +18,7 @@ import { AuthContext } from "../context/AuthContext";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native"; // <-- Only this is needed
 import { EventContext } from "../EventContext";
+import { API_URL } from "../config";
 import { resolveImageUrl } from "../utils/imageSource";
 
 const AVATAR_PLACEHOLDER = "https://placehold.co/80x80?text=User";
