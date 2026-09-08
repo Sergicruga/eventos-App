c62427c298dd4c3db4791daf4a7063fc1ecc6f8e
Sergicruga <sergicruga@gmail.com>
2026-09-08 13:43:27 +0200

    Soportar DATABASE_URL en Render

--- backend/server.js
+++ backend/server.js
@@ -108,14 +108,21 @@ const uploadEventImage = multer({ storage: eventStorage });
 
 const isProd = process.env.NODE_ENV === "production";
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT || 5432),
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  ssl: { rejectUnauthorized: false }, // obligatorio con Render
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }, // obligatorio con Render
+      }
+    : {
+        host: process.env.PGHOST,
+        port: Number(process.env.PGPORT || 5432),
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        ssl: { rejectUnauthorized: false }, // obligatorio con Render
+      }
+);
 
 pool
   .connect()
